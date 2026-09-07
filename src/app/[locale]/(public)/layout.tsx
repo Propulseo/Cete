4e4e58103464ccfe3c49cff985af7f25ae9c004c
Propulseo <team@propulseo-site.com>
2026-09-07 17:45:41 +0200

    feat(analytics): add Umami to the public site

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { setRequestLocale } from "next-intl/server";
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
@@ -17,6 +18,16 @@ export default async function PublicLayout({
 
   return (
     <>
+      <Script
+        id="umami-analytics"
+        src="https://stats.propulseo-site.com/script.js"
+        strategy="afterInteractive"
+        data-website-id="5d5d1d6f-72ce-4d74-a84d-7566442e39ee"
+        data-domains="cet-notation.com,www.cet-notation.com"
+        data-exclude-search="true"
+        data-exclude-hash="true"
+        data-do-not-track="true"
+      />
       <JsonLd data={organizationJsonLd(locale as Locale)} />
       <Header />
       <main className="flex-1">{children}</main>
